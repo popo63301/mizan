f6e9fcebf2cdb6f1b6b006c24af3249a06c29778
popo63301 <popo63301@gmail.com>
2018-05-29 19:02:12 +0200

    Uncomment Interstitial

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { Text, StatusBar, Platform } from 'react-native';
+import React from 'react';
+import { Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { AdMobInterstitial } from 'react-native-admob';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import IntlProvider from './src/containers/IntlProvider';
@@ -9,44 +8,14 @@ import SplashScreen from './src/components/SplashScreen';
 import Router from './src/router';
 import { store, persistor } from './src/redux/configureStore';
 
-class App extends Component {
-  componentDidMount() {
-    // this.showInterstitial();
-    StatusBar.setBackgroundColor('#528D6D');
-  }
-
-  showInterstitial = () => {
-    const os = Platform.OS;
-    switch (os) {
-      case 'android': {
-        AdMobInterstitial.setAdUnitID('ca-app-pub-5296245083041935/2344356925');
-        break;
-      }
-      case 'ios': {
-        AdMobInterstitial.setAdUnitID('ca-app-pub-5296245083041935/4284727191');
-        break;
-      }
-      default: {
-        AdMobInterstitial.setAdUnitID('ca-app-pub-5296245083041935/2344356925');
-        break;
-      }
-    }
-
-    AdMobInterstitial.requestAd()
-      .then(() => AdMobInterstitial.showAd())
-      .catch(e => console.log('problem Interstitial', e));
-  };
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={<SplashScreen />} persistor={persistor}>
-          <IntlProvider textComponent={Text}>
-            <Router />
-          </IntlProvider>
-        </PersistGate>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={<SplashScreen />} persistor={persistor}>
+      <IntlProvider textComponent={Text}>
+        <Router />
+      </IntlProvider>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { StatusBar, Platform, View } from 'react-native';
 import { connect } from 'react-redux';
 import { BackButton, NativeRouter, Route } from 'react-router-native';
+import { AdMobInterstitial } from 'react-native-admob';
+
 import HomePage from './components/HomePage';
 import CounterPage from './components/CounterPage';
 import OptionPage from './containers/OptionPage';
@@ -11,8 +13,33 @@ import { reinitCounters } from './redux/actions/counter';
 class Router extends Component {
   componentDidMount() {
     const { lastDate, reinitCounters } = this.props;
+    StatusBar.setBackgroundColor('#528D6D');
     this.props.reinitCounters(lastDate);
+    this.showInterstitial();
   }
+
+  showInterstitial = () => {
+    const os = Platform.OS;
+    switch (os) {
+      case 'android': {
+        AdMobInterstitial.setAdUnitID('ca-app-pub-5296245083041935/2344356925');
+        break;
+      }
+      case 'ios': {
+        AdMobInterstitial.setAdUnitID('ca-app-pub-5296245083041935/4284727191');
+        break;
+      }
+      default: {
+        AdMobInterstitial.setAdUnitID('ca-app-pub-5296245083041935/2344356925');
+        break;
+      }
+    }
+
+    AdMobInterstitial.requestAd()
+      .then(() => AdMobInterstitial.showAd())
+      .catch(e => console.log('problem Interstitial', e));
+  };
+
   render() {
     return (
       <NativeRouter>
